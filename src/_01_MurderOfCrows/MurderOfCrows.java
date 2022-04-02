e48fb856ed69fa83b9981832ff0cbe7a0cda7722
root <root@a8d750971e59>
2022-04-02 18:22:56 +0000

    Murder of crows(IP)

--- src/_01_MurderOfCrows/MurderOfCrows.java
+++ src/_01_MurderOfCrows/MurderOfCrows.java
@@ -17,7 +17,11 @@ public class MurderOfCrows {
         // 1. One of the Crows has eaten the diamond. You need to search
         //    through the stomach of each Crow, then print the name of the
         //    guilty Crow.
-
+for(int i=0; i< theMurder.size();i++) {
+	if(theMurder.get(i).getStomachContents() ==) {
+		
+	}
+}
         // 2. How many innocent crows had to die before the diamond was found?
 
     }
